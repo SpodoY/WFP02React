9b4417fe8a1ca33ab7e3d158053d27aebf6345f2
Thomas Winter <thommy2002@outlook.com>
2023-11-22 14:07:29 +0100

    feat: Seems to work now :)

--- src/routes/Results.jsx
+++ src/routes/Results.jsx
@@ -7,24 +7,24 @@ import NavBar from "../components/NavBar";
 import { cheerfulFiestaPalette } from "@mui/x-charts";
 import ElectionSol from "../artifacts/contracts/Election.sol/Election.json";
 
-const Result = ({contract_address}) => {
+const Result = ({ contract_address }) => {
   const [dataSet, setDataSet] = useState([]);
   const [provider, setProvider] = useState(null);
   const [contract, setContract] = useState(null);
 
   const contractAddress = contract_address;
 
-  const ABI = ElectionSol.abi
+  const ABI = ElectionSol.abi;
 
   const handleAccountsChanged = async () => {
-    let provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545/");
-    console.log(window.ethereum)
+    // SANITY CHECK
+    let metaProv = new ethers.BrowserProvider(window.ethereum);
+    let rpcProv = new ethers.JsonRpcProvider("http://127.0.0.1:8545/");
 
-    /**
-     * Creates a contract object with my contract address, the defined functions
-     * inside the ABI and as a provider -> we can't write just read yet
-     * */
-    setContract(new Contract(contractAddress, ABI, provider));
+    console.log("Metamask Prov");
+    console.log(await metaProv.getNetwork());
+    console.log("URL Prov");
+    console.log(await rpcProv.getNetwork());
   };
 
   const queryCandidates = async (contractObject) => {
@@ -48,46 +48,54 @@ const Result = ({contract_address}) => {
   };
 
   useEffect(async () => {
-      queryCandidates(contract)
-  }, [contract])
+    queryCandidates(contract);
+  }, [contract]);
 
   useEffect(async () => {
     // Checks if MetaMask is installed and either connects via MetaMask or fallback
     if (window.ethereum) {
       // This tracks MetaMask account changes and then updates all values
       window.ethereum.on("accountsChanged", handleAccountsChanged);
-      await handleAccountsChanged();
+      // await handleAccountsChanged();
 
       // Get permission to query accounts
-      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      await window.ethereum.request({ method: "eth_requestAccounts" });
 
       // Browser provider from MetaMask's provider
       const browserProvider = new ethers.BrowserProvider(window.ethereum);
-
       setProvider(browserProvider);
+
+      /**
+       * Creates a contract object with my contract address, the defined functions
+       * inside the ABI and as a provider -> we can't write just read yet
+       * */
+      setContract(new Contract(contractAddress, ABI, browserProvider));
     } else {
       // Since MetaMask is not installed try default provider
       setProvider(ethers.getDefaultProvider());
     }
+    return () => {
+      window.ethereum.removeListener("accountsChanged", handleAccountsChanged);
+    };
   }, []);
 
   useEffect(async () => {
-    if (provider) {
+    if (provider && contract) {
       const eventFilter = contract.filters.votedEvent();
 
       const eventListener = async () => {
         const events = await contract.queryFilter(eventFilter);
-        queryCandidates(contract)
-        console.log("Voted Events: ", events)
-      }
+        queryCandidates(contract);
+        console.log("Voted Events: ", events);
+      };
 
-      contract.on(eventFilter, eventListener)
+      contract.on(eventFilter, eventListener);
 
       return () => {
-        contract.off(eventFilter, eventListener)
-      }
+        contract.off(eventFilter, eventListener);
+      };
     }
-  }, [provider]);
+  }, [provider, contract]);
 
   return (
     <>
